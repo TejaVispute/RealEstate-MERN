42f6085c1a21e082db2a061928810a44e45dc5f0
Tejas <tejasvispute25@gmail.com>
2023-10-04 16:33:40 +0530

    mongoose connection established

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from "express";
-
+import mongoose from "mongoose"
+import dotenv from "dotenv";
+dotenv.config()
 const app = express();
+
+
+
+
+// mongodb connection settings
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("connection established")
+}).catch((err) => {
+    console.log(err)
+});
+
+
 
 app.listen(3000, () => {
     console.log("Server is listening on 3000")
