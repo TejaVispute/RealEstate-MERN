c819f95aa8dae0af699b1ba0e4065fc9be8de8a3
Tejas <tejasvispute25@gmail.com>
2023-10-24 16:45:47 +0530

    redux persist added inside redux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import { persistReducer, persistStore } from "redux-persist"
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({
+    user: userReducer
+})
+
+
+// key value pair of user that is stored in local storage
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 
 export const store = configureStore({
-    reducer: { user: userReducer },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: true,
+        serializableCheck: false,
     }),
 })
+
+
+export const persistor = persistStore(store)
