12bbd70c98909475fde832ee5a959e6e11461dc4
Tejas <tejasvispute25@gmail.com>
2023-10-30 22:00:31 +0530

    added delete user functonality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { useRef, useState, useEffect } from "react";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 import { app } from "../firebase.js";
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice.js";
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
 const Profile = () => {
     const fileRef = useRef(null);
@@ -73,6 +73,34 @@ const Profile = () => {
         }
 
     }
+
+
+    // for deleting user 
+
+    const handleDeleteUser = async () => {
+
+
+        try {
+            dispatch(deleteUserStart());
+
+            const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+                method: "DELETE",
+            })
+            const data = await res.json();
+
+            if (data.success === false) {
+                dispatch(deleteUserFailure(data.message));
+                return;
+            }
+
+            dispatch(deleteUserSuccess(data))
+        } catch (error) {
+            dispatch(deleteUserFailure(error.message))
+        }
+    }
+
+
+
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className='text-3xl font-bold text-center my-7'>Profile</h1>
@@ -108,7 +136,7 @@ const Profile = () => {
             </form>
 
             <div className="flex justify-between mt-5 font-bold">
-                <span className="text-red-700 cursor-pointer">Delete Account</span>
+                <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
                 <span className="text-red-700 cursor-pointer">Signout</span>
 
             </div>
