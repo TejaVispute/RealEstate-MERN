8a3304b1f4c6cd1c42c5c45f413a79f90793c992
Tejas <tejasvispute25@gmail.com>
2023-12-16 09:23:29 +0530

    completed listing UI Page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,7 +4,12 @@ import { useParams } from 'react-router-dom';
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
-import "swiper/css/bundle"
+import "swiper/css/bundle";
+import { IoLocationOutline } from "react-icons/io5";
+import { IoBedOutline } from "react-icons/io5";
+import { LuBath } from "react-icons/lu";
+import { LuParkingCircle } from "react-icons/lu";
+import { TbSofa } from "react-icons/tb";
 
 const Listing = () => {
     SwiperCore.use([Navigation]);
@@ -64,6 +69,58 @@ const Listing = () => {
                                 ))
                             }
                         </Swiper>
+
+                        {/* All Listing  */}
+                        <div className='py-[2vw] px-[5vw]'>
+
+                            <div>
+                                <h1 className='font-bold text-4xl '>{listing.name}</h1>
+                            </div>
+
+                            <div className='mt-5 flex items-center gap-2'>
+                                <IoLocationOutline className='text-4xl text-green-800' />  <h1 className=' text-md  '>{listing.address}</h1>
+                            </div>
+
+                            <div className='flex'>
+                                <div className='mt-5 text-white'>
+                                    {
+                                        listing.type === "rent" ? (
+                                            <p className='bg-red-800 py-2 px-5 text-xl rounded-md capitalize'>{listing.type}</p>
+                                        ) : <p className='bg-green-800 p-3'>{listing.type}</p>
+                                    }
+                                </div>
+
+                            </div>
+
+                            <div className='mt-5'>
+                                {
+                                    listing.description && <p>
+                                        <span className='text-xl font-bold'>Description :- </span>
+                                        {listing.description}
+
+                                    </p>
+                                }
+                            </div>
+
+
+                            {/*beds, bathroom ,  */}
+
+                            <div className='mt-5 flex gap-5'>
+                                <div className='flex items-center gap-2'>
+                                    <IoBedOutline className='text-xl' /> Beds {listing.bedrooms}
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <LuBath className='text-xl' /> Bath {listing.bathrooms}
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <LuParkingCircle className='text-xl' /> Parking {listing.parking}
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <TbSofa className='text-xl' /> Furnished {listing.furnished}
+                                </div>
+                            </div>
+
+                        </div>
                     </>
                 )
             }
