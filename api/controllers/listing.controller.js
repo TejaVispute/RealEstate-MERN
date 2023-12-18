8b9a33741a0a57f26a402cd49a26049c1e7ca92f
Tejas <tejasvispute25@gmail.com>
2023-12-18 20:25:04 +0530

    complete search API route functonality in backend

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -68,3 +68,117 @@ export const getListing = async (req, res, next) => {
         next(error);
     }
 }
+
+// export const getListings = async (req, res, next) => {
+//     console.log(req.query)
+//     try {
+//         const limit = parseInt(req.query.limit) || 10;
+//         const startIndex = parseInt(req.query.startIndex) || 0;
+//         let offer = req.query.offer;
+
+//         if (offer === undefined || offer === false) {
+//             offer = { $in: [false, true] }
+//         }
+
+//         let furnished = req.query.furnished;
+
+//         if (furnished === undefined || furnished === false) {
+//             furnished = { $in: [true, false] }
+//         }
+
+//         let parking = req.query.parking;
+
+//         if (parking === undefined || parking === false) {
+//             parking = { $in: [false, true] }
+//         }
+
+//         let type = req.query.type;
+
+//         if (type === undefined || type === "all") {
+//             type = { $in: ["sale", "rent"] }
+//         }
+
+//         let searchTerm = req.query.searchTerm || "";
+
+//         let sort = req.query.sort || "createdAt";
+
+//         let order = req.query.order || "desc";
+
+
+//         // find listing according to query
+
+//         const listings = await Listing({
+//             name: { $regex: searchTerm, $options: "i" },
+//             offer,
+//             furnished,
+//             parking,
+//             type,
+//         }).sort(
+//             { [sort]: order }
+//         ).limit(limit).skip(startIndex)
+
+//         return res.status(200).json(listings);
+
+
+
+
+//     } catch (error) {
+//         next(error)
+//     }
+// }
+
+export const getListings = async (req, res, next) => {
+    console.log(req.query);
+
+    try {
+        // Parse and set default values for pagination
+        const limit = parseInt(req.query.limit) || 10;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        // Parse and handle query parameters
+        let offer = req.query.offer;
+        if (offer === undefined || offer === false) {
+            offer = { $in: [false, true] };
+        }
+
+        let furnished = req.query.furnished;
+        if (furnished === undefined || furnished === false) {
+            furnished = { $in: [true, false] };
+        }
+
+        let parking = req.query.parking;
+        if (parking === undefined || parking === false) {
+            parking = { $in: [false, true] };
+        }
+
+        let type = req.query.type;
+        if (type === undefined || type === "all") {
+            type = { $in: ["sale", "rent"] };
+        }
+
+        let searchTerm = req.query.searchTerm || "";
+        let sort = req.query.sort || "createdAt";
+        let order = req.query.order || "desc";
+
+        // Build the query object based on parameters
+        const query = {
+            name: { $regex: searchTerm, $options: "i" },
+            offer,
+            furnished,
+            parking,
+            type,
+        };
+
+        // Find listings according to the query
+        const listings = await Listing.find(query)
+            .sort({ [sort]: order })
+            .limit(limit)
+            .skip(startIndex);
+
+        // Return the listings as JSON response
+        return res.status(200).json(listings);
+    } catch (error) {
+        // Handle errors and pass them to the error handling middleware
+        next(error);
+    }
+};
